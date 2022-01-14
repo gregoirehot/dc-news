6acf55be2697e5158b33fd33c22f2488e7367217
gregoire hot <gregoire.hot.partner@decathlon.com>
2022-01-14 17:56:09 +0100

    feat(router): init routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import './scss/index.scss'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/nav/Footer";
+import Header from "./components/nav/Header";
+import News from "./pages/News";
+import Timeline from "./pages/Timeline";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route
+          path={process.env.PUBLIC_URL + "/"}
+          element={<Timeline />}
+        />
+        <Route path={process.env.PUBLIC_URL + "/news/:id"} element={<News />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
