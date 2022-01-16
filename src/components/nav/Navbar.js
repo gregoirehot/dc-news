7f05199c854fcf7509cade50ed7dad96213b6f4e
gregoire hot <gregoire.hot.partner@decathlon.com>
2022-01-16 22:18:44 +0100

    feat(nav): css

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -3,18 +3,8 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarToggleExternalContent"
-        aria-controls="navbarToggleExternalContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light flex_between_content">
+      <div className="navbar-toggler-icon"></div>
       <Link className="navbar-brand" to={process.env.PUBLIC_URL + "/"}>
         DC-News
       </Link>
